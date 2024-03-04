c85874ba3b54b5184d8eb4ffdec1e78fdb99f827
cidiro <59629922+cidiro@users.noreply.github.com>
2024-03-04 19:56:51 +0100

    cc

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from puzzles.knights_tour import knights_tour
 # from puzzles.string_arrange import string_arrange
 
-# scramble "hello world!"
 if __name__ == '__main__':
     # string_arrange("olhel", "hello")
     # string_arrange("enigami", "imagine")
